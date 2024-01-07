1cde60a36a853abc0565e1965ba1ffb06cbe7b70
AgnieszkaMarczak <agnieszka.aurelia.marczak@gmail.com>
2024-01-07 20:28:19 +0100

    Reading passwords from file added

--- src/Game.java
+++ src/Game.java
@@ -1,65 +1,30 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Game {
-    static String[] polscyAktorzyIAktorki =
-            {"Andrzej Grabowski", "Janusz Gajos", "Anna Przybylska", "Franciszek Pieczka",
-                    "Pola Negri", "Borys Szyc", "Joanna Kulig", "Jan Englert",
-                    "Joanna Koroniewska", "Anita Sokołowska", "Katarzyna Figura", "Anna Dereszowska",
-                    "Małgorzata Kożuchowska", "Adrianna Biedrzyńska", "Barbara Brylska", "Wacław Kowalski",
-                    "Artur Żmijewski", "Adam Fidusiewicz", "Jerzy Cnota", "Mieczysław Hryniewicz"};
-    static String[] geografiaSwiata =
-            {"Mauna Kea", "Morze Żółte", "Morze Arafura", "Ramla Bay",
-                    "Sognefjorden", "Nanga Parbat", "Orinoko", "Zambezi",
-                    "Rzeka Świętego Wawrzyńca", "Rio de Janeiro", "Belo Horizonte", "Kuala Lumpur",
-                    "Ponta Delgada", "Dolina Śmierci", "Góry Brooksa", "Cieśnina Maltańska",
-                    "Góry Kaledońskie", "Zatoka Botnicka", "Wielka Nizina Węgierska", "Góry Kantabryjskie"};
-    static String[] jedzenie =
-            {"Śledź pod pierzynką", "Sałatka jarzynowa", "Gulasz segedyński", "Ratatouille",
-                    "Grzyby marynowane", "Czeburek", "Pierogi ruskie", "Zupa z dyni",
-                    "Ciasto drożdżowe", "Omlet biszkoptowy", "Sznycel wiedeński", "Mizeria",
-                    "Placek zbójnicki", "Placki ziemniaczane", "Jajka po benedyktyńsku", "Potrawka z królika",
-                    "Kiszka ziemniaczana", "Zupa mleczna", "Wodzianka", "Barszcz ukraiński"};
-    static String[] zwierzeta =
-            {"Zając szarak", "Mysz domowa", "Morświn", "Norka amerykańska",
-                    "Jeleń szlachetny", "Tchórz stepowy", "Mewa kanadyjska", "Koliberek czarnobrody",
-                    "Sikora dwubarwna", "Wrona meksykańska", "Goryl górski", "Hipopotam nilowy",
-                    "Słoń afrykański", "Wydra europejska", "Zebra sawannowa", "Dingo australijski ",
-                    "Wombat tasmański", "Kangur rdzawoszyi", "Dziobak australijski", "Mrówkożer workowaty"};
-    static String[] rosliny =
-            {"Buk pospolity", "Daglezja zielona", "Klon jawor", "Lipa szerokolistna",
-                    "Modrzew europejski", "Robinia akacjowa", "Sosna wejmutka", "Topola osika",
-                    "Wiąz szypułkowy", "Wierzba biała", "Dziewanna fioletowa", "Fiołek ogrodowy",
-                    "Goździk majowy", "Malwa różowa", "Niezapominajka", "Szałwia błyszcząca",
-                    "Begonia królewska", "Filodendron", "Różanecznik indyjski", "Lubczyk ogrodowy"};
     static String[] categories = {"Polscy Aktorzy i Aktorki", "Geografia Świata", "Jedzenie",
             "Zwierzęta", "Rośliny"};
-    static String[] passwordsFromFile = new String[20];
+    static String[] selectedCategoryFromFile = new String[20];
     static char[] drawnPassword;
     static char[] tempPassword;
     static int numberOfUnsuccessfulAttempts = 0;
-
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        readPasswordsFromFile("polscy_aktorzy");
-        System.out.println(Arrays.toString(passwordsFromFile));
-        System.exit(0);
         prepareGame();
         playGame();
     }
 
-    static String[] readPasswordsFromFile(String fileName) {
+    static String[] readCategoryPasswordsFromFile(String fileName) {
         try {
             Scanner sc = new Scanner(new File(fileName + ".csv"));
             int arrayIndex = 0;
             while (sc.hasNextLine()) {
                 String line = sc.nextLine();
                 String password = line.substring(0, line.length() - 1);
-                passwordsFromFile[arrayIndex] = password;
+                selectedCategoryFromFile[arrayIndex] = password;
                 arrayIndex++;
             }
         } catch (FileNotFoundException e) {
@@ -95,8 +60,9 @@ public class Game {
     static void preparePassword() {
         String selectedCategory = selectCategory();
         scanner.nextLine();
-        String password = drawPassword(selectedCategory);
-        System.out.println(password);
+        readCategoryPasswordsFromFile(selectedCategory);
+        String password = drawPassword(selectedCategoryFromFile);
+     //   System.out.println(password);
         drawnPassword = createCharArrayFromPassword(password);
         prepareTempPassword();
     }
@@ -108,7 +74,14 @@ public class Game {
                     "Podaj kategorię:");
             return selectCategory();
         }
-        return categories[category - 1];
+        return switch(category){
+            case 1 -> "polscy_aktorzy";
+            case 2 -> "geografia_swiata";
+            case 3 -> "jedzenie";
+            case 4 -> "zwierzeta";
+            case 5 -> "rosliny";
+            default -> selectCategory();
+        };
     }
 
     static String drawPassword(String[] category) {
